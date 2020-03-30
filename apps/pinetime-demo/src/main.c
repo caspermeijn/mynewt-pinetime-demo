f19bd951c0a1516fe6cbe4a53e91c6aa5ab68241
Casper Meijn <casper@meijn.net>
2020-03-30 19:47:26 +0200

    Add charge control

--- apps/pinetime-demo/src/main.c
+++ apps/pinetime-demo/src/main.c
@@ -22,9 +22,55 @@
 #include "os/os_callout.h"
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
+#include "sgm4056/sgm4056.h"
+#include "console/console.h"
 #ifdef ARCH_sim
 #include "mcu/mcu_sim.h"
 #endif
+
+static int 
+charge_control_data_callback(struct charge_control *chg_ctrl, void * arg,
+        void *data, charge_control_type_t type) 
+{
+    if (type == CHARGE_CONTROL_TYPE_STATUS) {
+        charge_control_status_t charger_status = *(charge_control_status_t*)(data);
+
+        console_printf("Charger state is ");
+        if (charger_status == CHARGE_CONTROL_STATUS_NO_SOURCE) {
+            console_printf("no source detected\n");
+        } else if (charger_status == CHARGE_CONTROL_STATUS_CHARGING) {
+            console_printf("charging\n");
+        } else if (charger_status == CHARGE_CONTROL_STATUS_CHARGE_COMPLETE) {
+            console_printf("charge completed\n");
+        } else {
+            assert(false);
+        }
+    } else {
+        assert(false);
+    }
+    console_flush();
+    return 0;
+}
+
+struct charge_control *charge_control;
+
+struct charge_control_listener charge_control_listener = {
+    .ccl_type = CHARGE_CONTROL_TYPE_STATUS,
+    .ccl_func = charge_control_data_callback,
+};
+
+static void 
+charge_control_init(void)
+{
+    charger = charge_control_mgr_find_next_bytype(CHARGE_CONTROL_TYPE_STATUS, NULL);
+    assert(charger);
+
+    rc = charge_control_set_poll_rate_ms("charger", 10000);
+    assert(rc == 0);
+
+    rc = charge_control_register_listener(charger, &charger_listener);
+    assert(rc == 0);
+}
 
 static void periodic_callback(struct os_event *ev);
 static struct os_callout periodic_callout;
@@ -47,6 +93,7 @@ static void periodic_callback(struct os_event *ev)
 {
     int rc;
 
+    /* Toggle blinking led */
     hal_gpio_toggle(LED_BLINK_PIN); 
 
     /* Trigger in another second */
@@ -66,6 +113,7 @@ main(int argc, char **argv)
     sysinit();
 
     periodic_init();
+    charge_control_init();
 
     while (1) {
        os_eventq_run(os_eventq_dflt_get());
